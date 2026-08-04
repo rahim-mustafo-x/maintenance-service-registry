4c6e1b71127f51e17ec7db9bce22c0a79a99af63
rahim-mustafo-x <rahim.mustafo.x@gmail.com>
2026-08-05 00:45:56 +0500

    eureka Server registery

---
+++ src/test/java/org/safa/maintenanceserviceregistery/MaintenanceServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.safa.maintenanceserviceregistery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceServiceRegisteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
