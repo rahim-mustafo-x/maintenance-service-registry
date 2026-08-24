951129c17c32b5edc9a97880dbeeed91e90eed6e
rahim-mustafo-x <rahim.mustafo.x@gmail.com>
2026-08-24 12:40:13 +0500

    fuck the deploy.yml

--- src/main/java/org/safa/maintenanceserviceregistery/MaintenanceServiceRegisteryApplication.java
+++ src/main/java/org/safa/maintenanceserviceregistery/MaintenanceServiceRegisteryApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class MaintenanceServiceRegisteryApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(MaintenanceServiceRegisteryApplication.class, args);
     }
 
